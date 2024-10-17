3bb104ad79798e4974c41bcac0d6b107eb0358a2
Akash Mohite <96733456+akashmohite17021998@users.noreply.github.com>
2024-10-17 14:09:09 +0530

    Added student class

--- Main.java
+++ Main.java
@@ -7,6 +7,8 @@ public class Main{
         System.out.println(new Date());
         System.out.println("Hello");
         System.out.println("Added one more file");
+        Student stu = new Student();
+        stu.sayHello();
     }
 
 }
